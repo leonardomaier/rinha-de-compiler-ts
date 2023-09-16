0a7ccd8b151407b940ace4611459081a6643a841
Leonardo Maier <leonardomaier@goamify.com>
2023-09-16 11:09:17 -0300

    adds int type and support to print integers

--- src/main.ts
+++ src/main.ts
@@ -7,43 +7,57 @@ type SourceLocation = {
 };
 
 type Str = {
-    kind: string,
+    kind: string;
     value: string;
     location: SourceLocation;
 };
+
+type Int = {
+    kind: string;
+    value: number;
+    location: SourceLocation;
+}
 
 type Print = {
     kind: string;
     value: Term;
 };
 
-type Types = Str | Print;
+type Types = Str | Int | Print;
 
 enum TermKindEnum {
     Str = "Str",
-    Print = "Print"
+    Print = "Print",
+    Int = "Int"
 };
 
 interface Term { [key: string]: any }
 
-function struct<T>(params: T): T {
-    return params;
+function struct<T>(term: Term): T {
+    return {
+        value: term.value,
+        kind: term.kind,
+        location: term.location
+    } as T; 
 }
 
 function evaluate(term: Term): Types {
 
     switch(term.kind) {
         case TermKindEnum.Str:
-            return struct<Str>({ 
-                value: term.value.toString(), 
-                kind: term.kind,
-                location: term.location 
-            });
+            return struct<Str>(term);
+        case TermKindEnum.Int:
+            return struct<Int>(term);
         case TermKindEnum.Print:
             const { value, kind } = evaluate(term.value);
 
             if (kind === TermKindEnum.Str) {
                 console.log(`${value}`);
+                return;
+            }
+
+            if (kind === TermKindEnum.Int) {
+                console.log(+value);
                 return;
             }
 
@@ -53,11 +67,8 @@ function evaluate(term: Term): Types {
 }
 
 function main() {
-
-    const stdin = fs.readFileSync('./examples/print.json');
-
+    const stdin = fs.readFileSync('./examples/print_int.json');
     const program = JSON.parse(stdin.toString());
-
     console.log(evaluate(program.expression));
 }
 
